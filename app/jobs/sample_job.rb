5c3a7839743f3081d8d52b6d6b059ef6b42e8b4b
nakayama <aki.gkatay.2020@gmail.com>
2019-07-26 17:04:39 +0900

    feat: change

--- app/jobs/sample_job.rb
+++ app/jobs/sample_job.rb
@@ -4,6 +4,6 @@ class SampleJob < ApplicationJob
   def perform(*args)
     puts "The number of users is #{User.count} people!"
     notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
-    notifier.ping("17:00にお知らせを送るよー！")
+    notifier.ping("17:10にお知らせを送るよー！")
   end
 end
