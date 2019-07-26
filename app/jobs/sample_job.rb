fbfdf83818bc48d3bf65a41eaaadfdc97f05a5f4
nakayama <aki.gkatay.2020@gmail.com>
2019-07-26 16:47:33 +0900

    feat: jobを追加

---
+++ app/jobs/sample_job.rb
@@ -0,0 +1,9 @@
+class SampleJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    puts "The number of users is #{User.count} people!"
+    notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
+    notifier.ping("17:00にお知らせを送るよー！")
+  end
+end
--- config/clock.rb
+++ config/clock.rb
@@ -3,11 +3,17 @@ require File.expand_path('../boot', __FILE__)
 require File.expand_path('../environment', __FILE__)
 
 module Clockwork
+  configure do |config|
+    config[:tz] = 'JST'
+  end
+
+  error_handler do |error|
+    Rails.logger.error("occur error")
+  end
+
   handler do |job|
     puts "Runnning #{job}"
   end
 
-  every(1.minute, "sample 1 minute") {
-    `rake task_sample:sample`
-  }
+  every(1.day, "sample notification", :at => '17:00') { SampleJob.perform_later }
 end
