5b644830a0bfea1c80453fc4d0ddffaae420bd0a
nakayama <aki.gkatay.2020@gmail.com>
2019-07-26 13:19:49 +0900

    feat: add clock file

---
+++ config/clock.rb
@@ -0,0 +1,11 @@
+require 'clockwork'
+require File.expand_path('../boot', __FILE__)
+require File.expand_path('../environment', __FILE__)
+
+module Clockwork
+  handler do |job|
+    puts "Runnning #{job}"
+  end
+
+  every(1.minute, "sample 1 minute")
+end
